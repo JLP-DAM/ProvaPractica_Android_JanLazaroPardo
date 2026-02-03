10e7cc2be2d651128ef27ae3c6208de0be8dc986
JLP-DAM <2223_jan.lazaro@iticbcn.cat>
2026-02-03 18:50:33 +0100

    Commit de progres, l'aplicacio compila i arreglat viewholder de forma temporal

--- app/src/main/java/com/example/provapractica/TascaViewHolder.kt
+++ app/src/main/java/com/example/provapractica/TascaViewHolder.kt
@@ -1,8 +1,17 @@
 package com.example.provapractica
 
 import android.view.View
+import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 
 class TascaViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+    //private val nameTextView: TextView = itemView.findViewById(R.id.name)
+    fun renderitza(item: Tasca) {
+        /*nameTextView.text = item.name
+        testTextView.text = item.text
 
+        itemView.setOnClickListener {
+            onItemClick(item)
+        }*/
+    }
 }
